be489bc986041045867dc13c28c47ed50579cc19
ototadana <ototadana@gmail.com>
2015-03-29 18:00:51 +0900

    change oneoff script implementation from `docker-compose` to `docker`

--- test/clean-containers.sh
+++ test/clean-containers.sh
@@ -14,17 +14,15 @@ assert()
 BASE_DIR=$(cd $(dirname $0); pwd)
 TEMP_DIR=${BASE_DIR}/temp
 
+if [ `docker ps |grep "pocci[s|n|r]_" |wc -l` -gt 0 ]; then
+    `docker ps |grep "pocci[s|n|r]_" |awk 'BEGIN{printf "docker stop "}{printf $1" "}'`
+fi
+if [ `docker ps -a |grep "pocci[s|n|r]_" |wc -l` -gt 0 ]; then
+    `docker ps -a |grep "pocci[s|n|r]_" |awk 'BEGIN{printf "docker rm "}{printf $1" "}'`
+fi
+
 if [ -d ${TEMP_DIR} ]; then
-    cd ${TEMP_DIR}/pocci/bin
-    echo 'y' | ./destroy-service
     rm -fr ${TEMP_DIR}
-else
-    if [ `docker ps |grep "pocci[s|n|r]_" |wc -l` -gt 0 ]; then
-        `docker ps |grep "pocci[s|n|r]_" |awk 'BEGIN{printf "docker stop "}{printf $1" "}'`
-    fi
-    if [ `docker ps -a |grep "pocci[s|n|r]_" |wc -l` -gt 0 ]; then
-        `docker ps -a |grep "pocci[s|n|r]_" |awk 'BEGIN{printf "docker rm "}{printf $1" "}'`
-    fi
 fi
 
 assert poccis_
