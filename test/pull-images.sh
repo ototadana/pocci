22280f3c5773603f91cc52ad61939a936568a305
ototadana <ototadana@gmail.com>
2015-04-21 20:15:38 +0900

    Add jenkins-slave-base to pull-images.sh

--- test/pull-images.sh
+++ test/pull-images.sh
@@ -6,6 +6,7 @@ docker pull ototadana/phpldapadmin
 docker pull ototadana/sonarqube
 docker pull ototadana/alminium
 docker pull ototadana/jenkins
+docker pull ototadana/jenkins-slave-base
 docker pull ototadana/jenkins-slave-nodejs
 docker pull ototadana/jenkins-slave-iojs
 docker pull ototadana/jenkins-slave-java
